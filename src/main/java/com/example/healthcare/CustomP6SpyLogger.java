5a265a52944a64edc572d855e7fcbf4a41632639
AnteBrkicEpam <ante_brkic@epam.com>
2025-04-08 10:49:16 +0200

    Added docker for oracle

--- src/main/java/com/example/healthcare/CustomP6SpyLogger.java
+++ src/main/java/com/example/healthcare/CustomP6SpyLogger.java
@@ -4,11 +4,11 @@ import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
 
 public class CustomP6SpyLogger implements MessageFormattingStrategy {
 
-    private static final long THRESHOLD = 50; // Threshold in milliseconds
+    private static final long THRESHOLD = 0; // Threshold in milliseconds
 
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
-        if (elapsed > THRESHOLD) {
+        if (elapsed >= THRESHOLD) {
             return String.format("Elapsed Time: %d ms | Category: %s | Connection: %d | Prepared: %s | SQL: %s",
                     elapsed, category, connectionId, prepared, sql);
         }
