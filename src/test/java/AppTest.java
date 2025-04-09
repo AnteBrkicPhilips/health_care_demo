80105f973c0ec3961a25f4bb04b81c755aae9d23
Oleg Dorodnykh <oleg_dorodnykh@epam.com>
2025-04-09 22:58:47 +0300

    test

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,10 @@
+import com.example.healthcare.HealthcareSystem;
+import org.junit.Test;
+
+public class AppTest {
+
+    @Test
+    public void testApp(){
+        HealthcareSystem.main(null);
+    }
+}
